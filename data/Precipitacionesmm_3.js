502c14d799e118cfd0c5d6f5bff42d69dfab06a5
pattyqr <patricia.quevedo@unmsm.edu.pe>
2021-11-09 16:34:40 -0500

    09112021

---
+++ data/Precipitacionesmm_3.js
@@ -0,0 +1,25 @@
+var json_Precipitacionesmm_3 = {
+"type": "FeatureCollection",
+"name": "Precipitacionesmm_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "LAT": -11.95171, "LONG": -76.78592, "ACUM": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.785921, -11.951709 ] } },
+{ "type": "Feature", "properties": { "LAT": -11.92285, "LONG": -76.70208, "ACUM": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.70208, -11.922848 ] } },
+{ "type": "Feature", "properties": { "LAT": -11.92525, "LONG": -76.68669, "ACUM": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.686694, -11.92525 ] } },
+{ "type": "Feature", "properties": { "LAT": -11.99437, "LONG": -76.81674, "ACUM": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.816737, -11.994373 ] } },
+{ "type": "Feature", "properties": { "LAT": -11.94618, "LONG": -76.69513, "ACUM": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.695133, -11.946182 ] } },
+{ "type": "Feature", "properties": { "LAT": -11.7438, "LONG": -76.6089, "ACUM": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.608897, -11.743797 ] } },
+{ "type": "Feature", "properties": { "LAT": -11.91628, "LONG": -76.66658, "ACUM": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.666583, -11.916278 ] } },
+{ "type": "Feature", "properties": { "LAT": -11.97089, "LONG": -76.75422, "ACUM": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.754222, -11.970889 ] } },
+{ "type": "Feature", "properties": { "LAT": -11.89497, "LONG": -76.65222, "ACUM": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.652222, -11.894972 ] } },
+{ "type": "Feature", "properties": { "LAT": -11.46693, "LONG": -76.62463, "ACUM": 5.0 }, "geometry": { "type": "Point", "coordinates": [ -76.624634, -11.466927 ] } },
+{ "type": "Feature", "properties": { "LAT": -11.80656, "LONG": -76.32431, "ACUM": 3.5 }, "geometry": { "type": "Point", "coordinates": [ -76.324309, -11.806563 ] } },
+{ "type": "Feature", "properties": { "LAT": -11.98944, "LONG": -76.82006, "ACUM": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.820056, -11.989444 ] } },
+{ "type": "Feature", "properties": { "LAT": -11.93874, "LONG": -76.70245, "ACUM": 2.0 }, "geometry": { "type": "Point", "coordinates": [ -76.702445, -11.9387378 ] } },
+{ "type": "Feature", "properties": { "LAT": -11.97697, "LONG": -76.74489, "ACUM": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.744889, -11.976972 ] } },
+{ "type": "Feature", "properties": { "LAT": -11.92092, "LONG": -76.67036, "ACUM": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.670361, -11.920917 ] } },
+{ "type": "Feature", "properties": { "LAT": -11.97958, "LONG": -76.83186, "ACUM": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.831861, -11.979583 ] } },
+{ "type": "Feature", "properties": { "LAT": -11.90771, "LONG": -76.67326, "ACUM": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.673259, -11.907712 ] } },
+{ "type": "Feature", "properties": { "LAT": -11.92732, "LONG": -76.68521, "ACUM": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.685214, -11.927318 ] } }
+]
+}
